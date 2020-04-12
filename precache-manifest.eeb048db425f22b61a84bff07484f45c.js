c2c377540f8fda8de4b584b711e4192e151c03fb
zhangrp <zhangrp@bairuitech.com>
2020-04-12 15:01:15 +0800

    deploy
    Change-Id: I8da20d4e80521dec70d847ade84eceedfe22a8de

---
+++ precache-manifest.eeb048db425f22b61a84bff07484f45c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07c721e5f688889306229c8768d192c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "af89c64a1880ece98523",
+    "url": "./static/css/main.61c0caa4.chunk.css"
+  },
+  {
+    "revision": "466894a839cf52f05a06",
+    "url": "./static/js/2.a7a0e4b8.chunk.js"
+  },
+  {
+    "revision": "ced5f5b7d16291925a659536303b3fc6",
+    "url": "./static/js/2.a7a0e4b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af89c64a1880ece98523",
+    "url": "./static/js/main.e27ad0ef.chunk.js"
+  },
+  {
+    "revision": "d97f3af156631cdc50ae",
+    "url": "./static/js/runtime-main.acd2660e.js"
+  }
+]);
